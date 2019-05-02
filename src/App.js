ce80e07749b80c35605305c3d65df6fe1eb014c1
Alexis K Grisham <alexisgrisham@AlexisGMac.localdomain>
2019-05-02 13:20:01 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+
+function App() {  
+  constructor(props) {
+    super(props);
+    this.state = {
+    rolls: [],
+    rollSum: null,
+  } 
+}
+
+  Roll20 = () => {
+    let rolls = [];
+    let rollSum = 1;
+    for (let i = 0; i < 20; i++) {
+      rolls[i] = Math.floor(Math.random() * 20) + 1;
+      rollSum += rolls[i];
+    }
+    this.setState({
+       rolls,
+        rollSum})
+
+
+    // Math.random()*(20-1) + 1;
+    // return display.getElementById(results)
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <header><h1>Dungeons and Dragons!</h1></header>
+ <nav>
+   <button onClick={this.Roll20}>D20</button>
+   <button>D12</button>
+   <button>D10</button>
+   <button>D10%</button>
+   <button>D8</button>
+   <button>D6</button>
+   <button>D4</button>
+   </nav> 
+   
+     <section>
+  {[1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(number => {
+    return (
+      <button
+        key={number}
+        onClick={() => this.diceRoll(number)}
+
+      >
+        {this.props.number}
+        </button>
+    );
+  })}
+     </section>
     </div>
   );
 }
