c89a38afbae98df8b7a4a53f41c6059f80e29ece
Alexis K Grisham <alexisgrisham@AlexisGMac.localdomain>
2019-05-02 16:17:14 -0400

    beforestyling

--- src/App.js
+++ src/App.js
@@ -8,45 +8,97 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentNumber: undefined,
-      currentRoll:[],
+      currentIcosagon: undefined,
+      currentDodecagon: undefined,
+      currentDecagon: undefined,
+      currentHectogon: undefined,
+      currentOctogon: undefined,
+      currentHexagon: undefined,
+      currentQuadrilateral: undefined
     } 
   }
 
-
-    
-  //  Roll20 = () => {
-  //    let numbers = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"];
-  //    let roll = [];
-  //     Math.floor(Math.random(numbers)); 
-  //         roll.push({
-  //           number: numbers
-  //         });
-  //     };
-
-
-    setNumber = () => {
+    setIcosagon = () => {
       this.setState({
-        currentNumber: Math.ceil(Math.random() *  20)
+        currentIcosagon: Math.ceil(Math.random() *  20)
       }, () => {
-        console.log(this.state.currentNumber)
+        console.log('D20:' + this.state.currentIcosagon)
       })
     }
+
+    setDodecagon = () => {
+      this.setState({
+        currentDodecagon: Math.ceil(Math.random() *  12)
+      }, () => {
+        console.log('D12:' + this.state.currentDodecagon)
+      })
+    }
+
+
+    setDecagon = () => {
+      this.setState({
+        currentDecagon: Math.ceil(Math.random() *  10)
+      }, () => {
+        console.log('D10:' + this.state.currentDecagon)
+      })
+    }
+
+
+    setHectogon = () => {
+      this.setState({
+        currentHectogon: Math.ceil(Math.random() *  100)
+      }, () => {
+        console.log('D100:' + this.state.currentHectogon)
+      })
+    }
+
+
+    setOctogon = () => {
+      this.setState({
+        currentOctogon: Math.ceil(Math.random() *  8)
+      }, () => {
+        console.log('D8:' + this.state.currentOctogon)
+      })
+    }
+
+
+    setHexagon = () => {
+      this.setState({
+        currentHexagon: Math.ceil(Math.random() *  6)
+      }, () => {
+        console.log('D6:' + this.state.currentHexagon)
+      })
+    }
+
+
+    setQuadrilateral = () => {
+      this.setState({
+        currentQuadrilateral: Math.ceil(Math.random() *  4)
+      }, () => {
+        console.log('D4:'+ this.state.currentQuadrilateral)
+      })
+    }
+
 
    render() {
   return (
     <div className="App">
  <header><h1>Dungeons and Dragons!</h1></header>
  <nav>
-   <button onClick={this.setNumber}>D20</button>
-   {/* <span>{this.props.roll}</span> */}
-   <span>{this.state.currentNumber}</span>
-   <button>D12</button>
-   <button>D10</button>
-   <button>D10%</button>
-   <button>D8</button>
-   <button>D6</button>
-   <button>D4</button>
+   <button onClick={this.setIcosagon}>D20</button>
+   <span>{this.state.currentIcosagon}</span>
+   <button onClick={this.setDodecagon}>D12</button>
+   <span>{this.state.currentDodecagon}</span>
+   <button onClick={this.setDecagon}>D10</button>
+   <span>{this.state.currentDecagon}</span>
+   <button onClick={this.setHectogon}>D100</button>
+   <span>{this.state.currentHectogon}</span>
+   <button onClick={this.setOctogon}>D8</button>
+   <span>{this.state.currentOctogon}</span>
+   <button onClick={this.setHexagon}>D6</button>
+   <span>{this.state.currentHexagon}</span>
+   <button onClick={this.setQuadrilateral}>D4</button>
+   <span>{this.state.currentQuadrilateral}</span>
    </nav> 
    
 
